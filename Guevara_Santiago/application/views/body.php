50ef28f6320f3d74e78ec1d7866f84d3971fb70f
mrcamssys <mrcamssys@gmail.com>
2025-08-21 17:51:26 -0500

    fix: se actualiza lo trabajado en clase 21082025-5:51

--- Guevara_Santiago/application/views/body.php
+++ Guevara_Santiago/application/views/body.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?> <!doctype html>
 <body>
-    <h1>Hello, world!</h1>
+    <h2>Hello, world!</h>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
   </body>
 </html>
